0b6c2557c58efe3348acf851b3f63397e57c290c
BAkhtiyar <cena61454@gmail.com>
2023-01-19 14:30:10 +0600

    My first commit for Parsing Soft

---
+++ actions_modul.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep 
+ 
+headers = {
+        "User-Agent": 
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+}
+
+def download(url):
+    resp = requests.get(url, stream = True)
+    r = open("C:\\Users\\Pavilion\\Desktop\\Python\\parser\\image\\" + url.split("/")[-1], "wb")
+    for value in resp.iter_content(1024*1024): 
+        r.write(value)
+    r.close()
+
+def get_url():
+    for page in range(1, 8):
+        url = f'https://scrapingclub.com/exercise/list_basic/?page={page}'
+        requests_data = requests.get(url, headers = headers)
+
+        soup = BeautifulSoup(requests_data.text, 'lxml')
+
+        array_data = soup.find_all("div", class_ = "col-lg-4 col-md-6 mb-4")
+
+        for data in array_data:
+            link_page = "https://scrapingclub.com" + data.find('a').get('href')
+            yield link_page
+
+def get_data_array():
+    for page_url in get_url(): 
+        #ПС: писать код таким образом - ХАРАМ.
+        requests_data = requests.get(page_url, headers = headers)
+
+        sleep(3)
+
+        soup = BeautifulSoup(requests_data.text, 'lxml')
+        data_page = soup.find("div", class_ = "card mt-4 my-4")
+    
+        link_img = "https://scrapingclub.com" + data_page.find('img', class_='card-img-top img-fluid').get('src')
+        name_ctl = data_page.find('h3', class_='card-title').text.replace("\n", "")
+        price_ctl = data_page.find('h4').text
+        text_ctl = data_page.find('p', class_="card-text").text
+    
+        download(link_img)
+        yield name_ctl, price_ctl, text_ctl, link_img
